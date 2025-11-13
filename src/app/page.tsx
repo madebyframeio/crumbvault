40e4bc975670b403ee43cc71a36f5abc3e727061
Awofala Gbolahan <edawofal@gmail.com>
2025-11-13 15:06:43 +0100

    Added formspree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import logo from '../../public/logo.png'
 import banner from '../../public/banner.png'
@@ -5,9 +7,33 @@ import gdpr from '../../public/gdpr.png'
 import oneline from '../../public/oneline.png'
 import { FaXTwitter } from "react-icons/fa6";
 import mail from '../../public/mail-line.svg'
+import Link from 'next/link'
+import { useForm, ValidationError } from '@formspree/react'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
   const date = new Date()
+  const [state, handleSubmit] = useForm("mblqlzep");
+  const [email, setEmail] = useState('')
+  
+  function TemporaryMessage() {
+      const [visible, setVisible] = useState(true);
+
+      useEffect(() => {
+        const timer = setTimeout(() => setVisible(false), 3000);
+        return () => clearTimeout(timer);
+      }, []);
+    
+      return visible ? (
+        <p className="pb-2 text-[#8664FF]">Thanks for joining the waitlist</p>
+      ) : null;
+  }
+   useEffect(() => {
+    if (state.succeeded) {
+      setEmail("");
+    }
+  }, [state.succeeded]);
+
   return (
     <div>
       <main className='flex flex-col justify-center items-center pt-12 lg:px-36 md:px-14 px-5 text-center'>
@@ -21,20 +47,25 @@ const Home = () => {
             </p>
           </div>
           <div className='w-full lg:px-36 md:px-38 mb-6'>
-            <form action="" className='flex flex-col md:flex-row gap-4 w-full lg:px-40 mb-4'>
+            <form onSubmit={handleSubmit} className='flex flex-col md:flex-row gap-4 w-full lg:px-42 mb-4'>
               <div className='relative md:w-2/3'>
                 <Image src={mail} alt='mail icon' className='absolute left-2.5 top-1/2 -translate-y-1/2 text-[#000000] font-black w-5 h-4' />
-                <input type="text" placeholder='e.g dominic@mail.com' className='p-3 pl-8 rounded-xl shadow-2xl w-full border-t border-t-[#F0F2F4] focus:outline-none' />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name='email' placeholder='e.g dominic@mail.com' className='py-3 pl-8 rounded-xl shadow-2xl w-full border-t border-t-[#F0F2F4]' />
+                <ValidationError prefix="Email" field="email" errors={state.errors} />
               </div>
-              <button type="submit" className='cursor-pointer md:w-1/3 p-3 bg-[#8664FF] text-white rounded-2xl'>Get Started</button>
+              <button type="submit" disabled={state.submitting} className='hover:bg-[#9a7ffa] flex items-center justify-center text-center cursor-pointer md:w-35 p-3 bg-[#8664FF] text-white rounded-2xl'>
+                Get Started 
+                <svg className='text-white w-6 h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path></svg>
+              </button>
             </form>
+            {state.succeeded && <TemporaryMessage />}
             <p>Join <span className='text-[#8664FF]'>30+</span> early adopters waiting for launch</p>
           </div>
-          <div className='mt-6 flex gap-1 bg-linear-to-r from-[#8664FF0D] to-[#F5C96C0D] py-2 px-4 text-left border border-[#D9B48F33] rounded-2xl lg:mx-80 md:mx-42'>
-            <div className='w-1/3'>
+          <div className='mt-6 flex gap-1 md:gap-0 bg-linear-to-r from-[#8664FF0D] to-[#F5C96C0D] py-2 px-4 text-left border border-[#D9B48F33] rounded-2xl lg:mx-80 md:mx-42'>
+            <div className='w-1/3 '>
               <p className='font-medium'>Love Cookies?</p>
             </div>
-            <div className='w-2/3'>
+            <div className='w-2/3 md:w-3/4'>
               <p className='text-[#929292] text-[15px]'>Share CrumbVault with friends and get early access perks!</p>
             </div>
           </div>
@@ -42,21 +73,21 @@ const Home = () => {
             <div className='border border-[#F0F2F4] p-3 px-5 text-[14px] flex flex-col justify-center items-center md:w-1/3 lg:w-72 h-56 rounded-3xl'>
                 <Image src={banner} alt='Beautiful Banners Image' className='w-20 h-20 mb-6' />
                 <div>
-                  <h2 className='font-medium text-[20px]'>Beautiful Banners</h2>
+                  <h2 className='font-medium text-[20px] mb-2'>Beautiful Banners</h2>
                   <p className='text-[#929292]'>Customizable consent widgets that match your brand perfectly</p>
                 </div>
             </div>
             <div className='border border-[#F0F2F4] p-3 px-5 text-[14px] flex flex-col justify-center items-center md:w-1/3 lg:w-72 h-56 rounded-3xl'>
                 <Image src={gdpr} alt='Beautiful Banners Image' className='w-20 h-20 mb-6' />
                 <div>
-                  <h2 className='font-medium text-[20px]'>GDPR & CCPA Ready</h2>
+                  <h2 className='font-medium text-[20px] mb-2'>GDPR & CCPA Ready</h2>
                   <p className='text-[#929292]'>Built-in compliance tools to keep you on the right side of privacy laws</p>
                 </div>
             </div>
             <div className='border border-[#F0F2F4] p-3 px-5 text-[14px] flex flex-col justify-center items-center md:w-1/3 lg:w-72 h-56 rounded-3xl'>
                 <Image src={oneline} alt='Beautiful Banners Image' className='w-20 h-20 mb-6' />
                 <div>
-                  <h2 className='font-medium text-[20px]'>One Line Setup</h2>
+                  <h2 className='font-medium text-[20px] mb-2'>One Line Setup</h2>
                   <p className='text-[#929292]'>Add CrumbVault to your site in seconds with our simple integration</p>
                 </div>
             </div>
@@ -67,13 +98,13 @@ const Home = () => {
           <p className='text-[#929292]'>&copy; {date.getFullYear()} Crumbvault</p>
         </div>
         <div className='md:pl-24 hidden md:block'>
-          <p><FaXTwitter className='cursor-pointer w-10 h-10 p-2 rounded-full text-white bg-black' /></p>
+          <Link target='_blank' rel='noopener noreferrer' href='https://x.com/usecrumbvault'><p><FaXTwitter className='cursor-pointer w-10 h-10 p-2 rounded-full text-white bg-black' /></p></Link>
         </div>
         <div>
-          <p>Questions?? <span className='text-[#8668ef]'>hello@crumbvault.com</span></p>
+          <Link rel='noopener noreferrer' href='mailto:hello@crumbvault.xyz'><p>Questions?? <span className='text-[#8668ef] hover:underline'>hello@crumbvault.xyz</span></p></Link>
         </div>
         <div className='md:pl-24 md:hidden'>
-          <p><FaXTwitter className='cursor-pointer md:w-8 md:h-8 md:p-1 w-10 h-10 p-2 rounded-full text-white bg-black' /></p>
+          <Link target='_blank' rel='noopener noreferrer' href='https://x.com/usecrumbvault'><p><FaXTwitter className='cursor-pointer md:w-8 md:h-8 md:p-1 w-10 h-10 p-2 rounded-full text-white bg-black' /></p></Link>
         </div>
       </footer>
     </div>
